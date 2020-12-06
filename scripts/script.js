fad40da821cc0c42e251cbe330c4cd1e2997b424
Matias Echeverria <mgecheverria@uc.cl>
2020-12-06 19:24:30 -0300

    map of chile

--- scripts/script.js
+++ scripts/script.js
@@ -165,6 +165,9 @@ const input = () => {
   body.append('input')
     .attr('id', 'transmitividad');
 
+  body.append('br');
+  body.append('br');
+
   body.append('button')
     .attr('class', 'button')
     .text('Calcular')
@@ -174,12 +177,6 @@ const input = () => {
       let r1 = document.getElementById('dist_vec').value;
       let s2 = document.getElementById('prof').value;
       let t = document.getElementById('transmitividad').value;
-
-      console.log(q);
-      console.log(s2);
-      console.log(r1);
-      console.log(s1);
-      console.log(t);
 
       d3.selectAll('p')
         .transition(1000)
@@ -198,6 +195,37 @@ const input = () => {
 
       calc(r1, s1, s2, q, t);
     });
+  
+  body.append('br');
+  body.append('br');
+
+  body.append('button')
+    .attr('class', 'button')
+    .text('Simular')
+    .on('click', () => {
+      let q = document.getElementById('caudal').value;
+      let s1 = document.getElementById('prof_vec').value;
+      let r1 = document.getElementById('dist_vec').value;
+      let s2 = document.getElementById('prof').value;
+      let t = document.getElementById('transmitividad').value;
+
+      d3.selectAll('p')
+        .transition(1000)
+        .attr('opacity', 0)
+        .remove();
+
+      d3.selectAll('input')
+        .transition(1000)
+        .attr('opacity', 0)
+        .remove();
+
+      d3.selectAll('button')
+        .transition(1000)
+        .attr('opacity', 0)
+        .remove();
+
+      simulacion(r1, s1, s2, q, t);
+    })
 }
 
 const calc = (r1, s1, s2, q, t) => {
@@ -328,5 +356,127 @@ const calc = (r1, s1, s2, q, t) => {
     .style('text-anchor', 'middle')
     .text('Profundidad pozo (m)')
 }
+
+const simulacion = async (r1, s1, s2, q, t) => {
+  const datos = await d3.csv('fluidos.csv');
+
+  let dist;
+  let curr = undefined;
+  let place;
+  datos.forEach((item) => {
+    curr = Math.sqrt(Math.pow(item['CAUDAL L/s'] - q,2)
+      + Math.pow(item['TRANSMISIVIDAD (m^2/d)'] - t,2));
+    if (dist == undefined) {
+      dist = curr;
+      place = item['NOMBRE']
+    }
+    if (dist > curr) {
+      dist = curr;
+      place = item['NOMBRE']
+    }
+  })
+
+  console.log(`Lugar: ${place} - Distancia: ${dist}`);
+  drawChile(datos);
+}
+
+const drawChile = async (datos) => {
+
+  const margin = {
+    top: 60,
+    right: 30,
+    bottom: 30,
+    left: 40
+  };
+  const width = 1000;
+  const height = 500;
+
+  const topeDensidad = 500;
+  d3.select('body')
+    .append('div')
+    .attr('id', 'svg-container');
+
+  const svgMap = d3
+    .select('#svg-container')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('class', 'map')
+
+  const containerMap = svgMap
+    .append('g')
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
+
+  const comunas = containerMap.append('g');
+
+  const title = svgMap.append('g');
+
+  title
+    .append('rect')
+    .attr('height', 30)
+    .attr('width', width)
+    .attr('fill', 'white')
+
+  title
+    .append('text')
+    .attr('x', width/2)
+    .attr('y', 20)
+    .attr('text-anchor', 'middle')
+    .attr('font-weight', 'bold')
+    .text('Acuíferos parecidos en Chile')
+  
+  const datosComunas = await d3.json('comunas.geojson');
+
+  let k;
+  const proyeccion = d3.geoMercator()
+    .fitSize([width, height], datosComunas);
+
+  const caminosGeo = d3.geoPath()
+    .projection(proyeccion);
+  let item;
+
+  const paths = comunas.selectAll('path')
+    .data(datosComunas.features)
+    .enter()
+    .append('path')
+    .attr('d', caminosGeo)
+    .attr('fill', 'blue')
+    .attr('opacity', 0.5)
+    .attr('stroke', 'red')
+    .attr('stroke-width', 1)
+
+  const circles = comunas
+    .selectAll('circles')
+    .data(datos)
+    .enter()
+    .append("circle")
+    .attr("cx", (d) => proyeccion([parseFloat(d['LATITUD']), parseFloat(d['LONGITUD'])])[0])
+    .attr("cy", (d) => proyeccion([parseFloat(d['LATITUD']), parseFloat(d['LONGITUD'])])[1])
+    .attr("r", 10)
+    .attr("fill", 'black');
+
+  const zoomHanlder = (event) => {
+    const transformation = event.transform;
+    k = transformation.k;
+    paths.attr("transform", transformation);
+    paths.attr("stroke-width", 1/k);
+    circles.attr("transform", transformation)
+      .attr("r", 10/k)
+  };
+
+  const zoom = d3.zoom()
+    .extent([
+        [0,0],
+        [width, height]
+    ])
+    .translateExtent([
+        [-50, -50],
+        [width + 50, height + 50],
+    ])
+    .scaleExtent([1, 30])
+    .on('zoom', zoomHanlder);
+
+  svgMap.call(zoom);
+};
 
 welcome();
